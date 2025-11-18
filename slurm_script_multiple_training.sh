dbcc3a33a5869f9d021a464285866a3523124cd4
ClouDens <isce2026.cloudens.ad@gmail.com>
2025-11-18 17:36:53 +0100

    update slurm script

--- slurm_script_multiple_training.sh
+++ slurm_script_multiple_training.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1 # 1 node
 #SBATCH --ntasks-per-node=32 # 32 tasks per node
 #SBATCH --time=24:00:00 # time limits: 1 hour
-#SBATCH --error=ibm.err # standard error file
-#SBATCH --output=ibm.out # standard output file
+#SBATCH --error=ibm_multiple.err # standard error file
+#SBATCH --output=ibm_multiple.out # standard output file
 #SBATCH --gres=gpu:a100:1
 #SBATCH --partition=gprod_gssi # partition name
 #SBATCH --mail-type=END              # type of event notification
