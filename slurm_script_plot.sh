6a95e71856016c0af9697366cfd1be0efbd94419
ClouDens <isce2026.cloudens.ad@gmail.com>
2025-11-18 13:03:08 +0100

    add slurm script for plot fpr

---
+++ slurm_script_plot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=AD_IBM
+#SBATCH --nodes=1 # 1 node
+#SBATCH --ntasks-per-node=32 # 32 tasks per node
+#SBATCH --time=24:00:00 # time limits: 1 hour
+#SBATCH --error=ibm.err # standard error file
+#SBATCH --output=ibm.out # standard output file
+#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=gprod_gssi # partition name
+#SBATCH --mail-type=END              # type of event notification
+#SBATCH --mail-user=thihoaithu.doan@gssi.it   # mail address
+
+module load python
+source torch210/bin/activate
+python src/plot_fpr.py
+deactivate
+
+
+
