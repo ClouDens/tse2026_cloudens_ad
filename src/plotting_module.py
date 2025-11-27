f094b4c0df5be1d938df04919ac1e2d22464ef0e
ClouDens <isce2026.cloudens.ad@gmail.com>
2025-11-27 17:36:25 +0100

    update

--- src/plotting_module.py
+++ src/plotting_module.py
@@ -453,8 +453,14 @@ def rename_columns_for_latex_display(df):
     for i in range(header_rows -1):
         row_list.append([np.nan] * df.shape[1])
     for index, row in df.iterrows():
+        row['Issue Tracker'] = f'{len(row['Issue Tracker'])}/3 \\\\' + '[' + ','.join(map(str,row['Issue Tracker']))+ ']'
+        row['Issue Tracker'] = wrap_text_by_multirowcell(row['Issue Tracker'], 2)
+        row['Instant Messenger'] = f'{len(row['Instant Messenger'])}/9 \\\\' + '[' + ','.join(map(str,row['Instant Messenger']))+ ']'
+        row['Instant Messenger'] = wrap_text_by_multirowcell(row['Instant Messenger'], 2)
+        row['Test Log'] = f'{len(row['Test Log'])}/7 \\\\' + '[' + ','.join(map(str,row['Test Log'])) + ']'
+        row['Test Log'] = wrap_text_by_multirowcell(row['Test Log'], 2)
         row_list.append(row.values.tolist())
-        row_rows = row['No. Subsets']
+        row_rows = max(row['No. Subsets'],2)
         for j in range(row_rows-1):
             row_list.append([np.nan] * df.shape[1])
     new_df = pd.DataFrame(row_list, columns=new_columns)
